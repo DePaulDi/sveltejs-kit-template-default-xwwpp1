d81d7c8e339b810b7e479f47d1a5c0579cb56e6a
DePaulDi <35300352+DePaulDi@users.noreply.github.com>
2023-05-17 09:49:21 +0200

    map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,17 +9,47 @@
 	let university = 'KU Leuven';
 	let number = 'r0877868';
   import { onMount } from 'svelte';
-  let data = [];
+  import data from "../../src/data/dataset1.json"
   let p_int = [];
   let imagePath = 'src/data/map_image3.jpg';
   let selectedData = [];
-
+  let jsonData;
 	
   onMount(async () => {
     const response = await fetch('src/data/dataset1.json');
     data = await response.json();
   });
 
+/*
+ $: {
+    if (data.length!=0) {
+      let minLat;
+      let maxLat;
+      let minLong;
+      let maxLong;
+      console.log("length");
+      console.log(data.length);
+      for (let i=0;i<data.length;i++)
+      {
+      minLat = Math.min(data[i].map(car => car.lat));
+      console.log(minLat);
+      maxLat = Math.max(data[i].map(car => car.lat));
+      console.log(maxLat);
+      minLong = Math.min(data[i].map(car => car.long));
+      console.log(minLong);
+      maxLong = Math.max(data[i].map(car => car.long));
+      console.log(maxLong);
+      }
+      const LONG_FACTOR = 600 / (maxLong - minLong);
+      const LAT_FACTOR = 600 / (maxLat - minLat);
+      console.log(LONG_FACTOR);
+      console.log(LAT_FACTOR);
+    }
+    else{
+      console.log("not_loaded");
+    }
+  }
+*/
  onMount(async () => {
     const response = await fetch('src/data/dataset2.json');
     p_int = await response.json();
@@ -60,11 +90,27 @@
     updateImage();
   });
  
-
   const MIN_LONGITUDE = 24.8825;
   const MAX_LATITUDE = 36.0675;
-  const LONGITUDE_TO_PIXEL_RATIO = 600 / 0.025;
-  const LATITUDE_TO_PIXEL_RATIO = 600 / 0.025;
+  
+  const mapWidth = 600;
+  const mapHeight = 600;
+
+  // Find the latitude and longitude range
+  const latitudes = data.map(car => car.lat);
+  const longitudes = data.map(car => car.long);
+  const minLatitude = Math.min(...latitudes);
+  const maxLatitude = Math.max(...latitudes);
+  const minLongitude = Math.min(...longitudes);
+  const maxLongitude = Math.max(...longitudes);
+
+  // Calculate the latitude and longitude differences
+  const latitudeRange = maxLatitude - minLatitude;
+  const longitudeRange = maxLongitude - minLongitude;
+
+  // Calculate the latitude and longitude ratios
+  const LATITUDE_TO_PIXEL_RATIO = mapHeight / latitudeRange;
+  const LONGITUDE_TO_PIXEL_RATIO = mapWidth / longitudeRange;
 </script>
 
 <style>
@@ -112,11 +158,12 @@
 		<li>Number: <b>{number}</b></li>
 	</ul>
 
- <svg width=60000 height=60000>
+ <svg width=600 height=600>
+  <rect x="0" y="0" width="600" height="600" fill="#efefef" />
   {#each data as car}
     <circle
-      cx={(car.long - MIN_LONGITUDE) * LONGITUDE_TO_PIXEL_RATIO}
-      cy={(MAX_LATITUDE - car.lat) * LATITUDE_TO_PIXEL_RATIO}
+      cx={(car.long - minLongitude) * LONGITUDE_TO_PIXEL_RATIO}
+      cy={(maxLatitude - car.lat) * LATITUDE_TO_PIXEL_RATIO}
       r="2"
       opacity="0.2"
       fill="black"
@@ -124,8 +171,8 @@
   {/each}
   {#each p_int as location}
     <circle
-      cx={(location.long - MIN_LONGITUDE) * LONGITUDE_TO_PIXEL_RATIO}
-      cy={(MAX_LATITUDE - location.lat) * LATITUDE_TO_PIXEL_RATIO}
+      cx={(location.long - minLongitude) * LONGITUDE_TO_PIXEL_RATIO}
+      cy={(maxLatitude - location.lat) * LATITUDE_TO_PIXEL_RATIO}
       r="10"
       fill={getLocationColor(location.type)}
       opacity = "1"
