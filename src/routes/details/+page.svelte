7f42c3bad0f90f1f17fd5c5d6e368e5af1a4f23f
DePaulDi <35300352+DePaulDi@users.noreply.github.com>
2023-05-18 11:05:06 +0200

    1 file modified

--- src/routes/details/+page.svelte
+++ src/routes/details/+page.svelte
@@ -19,6 +19,30 @@ onMount(() => {
 
 selectedCarId = param1;
 
+let selectedDay = 6;
+  let selectedMinute = 0;
+  let maxMinutes = 1440; // Total minutes in a day
+  let totalDays = 14; // Total number of days
+  let maxCumulativeMinutes = totalDays * maxMinutes;
+
+  let filteredData = [];
+
+  onMount(() => {
+    filterData();
+  });
+
+  function filterData() {
+    filteredData = dataset.filter(
+      (item) =>
+        item.day === selectedDay &&
+        item.cumulative_minute_total <= selectedMinute
+    );
+  }
+
+  function handleSliderChange(event) {
+    selectedMinute = parseInt(event.target.value);
+    filterData();
+  }
 </script>
 
 
@@ -31,4 +55,24 @@ selectedCarId = param1;
 
 <p>This is the current value {selectedCarId}</p>
 	
+	<div>
+  <input
+    type="range"
+    min="0"
+    max={maxCumulativeMinutes}
+    step="1"
+    bind:value={selectedMinute}
+    on:input={handleSliderChange}
+  />
+
+  <p>Selected Day: {selectedDay}</p>
+  <p>Selected Minute: {selectedMinute}</p>
+
+  <h2>Filtered Data:</h2>
+  <ul>
+    {#each filteredData as item (item.key)}
+      <li>{item.key}</li>
+    {/each}
+  </ul>
+</div>
 </main>
